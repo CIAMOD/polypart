54ee1257b3e8453b0f4d836c35e219cae683bdd8
Sergio Herreros <81583645+goomii17@users.noreply.github.com>
2025-09-22 13:05:44 +0200

    [PUBLISH]: published to test.pypi.org

--- polypart/__init__.py
+++ polypart/__init__.py
@@ -3,7 +3,7 @@ from .ppart import build_partition_tree, PartitionTree
 from .ftyping import as_fraction_vector, as_fraction_matrix
 from .io import save_tree, load_tree
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __all__ = [
     "Hyperplane",
     "Polytope",
